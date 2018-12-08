beb283e6229137f3374f906ec180a265a5fd217a
Julius Zerwick <jrzerwick@gmail.com>
2018-12-08 17:11:14 -0600

    Added Writing section, adjusted CSS, and modified fadein.js

--- src/fadein.js
+++ src/fadein.js
@@ -11,6 +11,7 @@ $(document).on("scroll", function() {
 	for (let i = 0; i < tags.length; i++) {
 		let tag = tags[i];
 
+		// if ($(tag).position().top < pageBottom && $(tag).position().top > pageTop) {
 		if ($(tag).position().top < pageBottom) {
 			$(tag).addClass("visible");
 		} else {
@@ -18,13 +19,21 @@ $(document).on("scroll", function() {
 		}
 	}
 
-	if ($("#personal").hasClass("visible")) {
+	if ($("#spacecraft").hasClass("visible")) {
 		$("html").css({ 'background-color': '#d85030', 'transition': 'background-color 500ms linear' });
-		$("#personal p").css({ 'color': '#b9c1cb' });
+		$("#spacecraft p").css({ 'color': '#b9c1cb' });
 		// if ($("#background").hasClass("visible") !== true) {
 		// 	$("html").css({'background-color': '#d85030'});
 		// }
 	} else {
 		$("html").css({ 'background-color': '#f7f7f7', 'transition': 'background-color 500ms linear' });
+	}
+
+	if ($("#essays").hasClass("visible")) {
+		$("html").css({ 'background-color': '#f7f7f7', 'transition': 'background-color 500ms linear' });
+		$("#essays a").css({ 'color': '#000' });
+		// if ($("#background").hasClass("visible") !== true) {
+		// 	$("html").css({'background-color': '#d85030'});
+		// }
 	}
 })
